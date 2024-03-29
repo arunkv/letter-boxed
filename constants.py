69f04cce9d3c05fb48eb7e0520639e7d86da6b46
Arun K Viswanathan <github@element77.com>
2024-03-29 10:13:59 +0530

    Pylint fixes

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
-#    Copyright 2024 Arun K Viswanathan
-#
-#    Licensed under the Apache License, Version 2.0 (the "License");
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-#
-#        http://www.apache.org/licenses/LICENSE-2.0
+"""
+This module defines constants used in the program.
+Copyright 2024 Arun K Viswanathan
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+"""
 
 MIN_WORD_LENGTH = 4
 MAX_WORD_LENGTH = float('inf')
